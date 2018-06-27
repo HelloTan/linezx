b9c2c1465228ed852e1bd2f7a7c55999ed823a35
Tanduri a.k.a HelloTan <34239334+HelloTan@users.noreply.github.com>
2018-06-27 12:49:53 +0700

    Update config.py

--- LINEZX/Api/config.py
+++ LINEZX/Api/config.py
@@ -3,21 +3,26 @@ from akad.ttypes import ApplicationType
 import re
 
 class Config():
-    LINE_HOST_DOMAIN            = 'https://legy-jp.line.naver.jp'
-    LINE_OBS_DOMAIN             = 'https://obs-sg.line-apps.com'
-    LINE_TIMELINE_API           = 'https://legy-jp.line.naver.jp/mh/api'
-    LINE_TIMELINE_MH            = 'https://legy-jp.line.naver.jp/mh'
+    LINE_HOST_DOMAIN                = 'https://legy-jp.line.naver.jp'
+    LINE_OBS_DOMAIN                 = 'https://obs-sg.line-apps.com'
+    LINE_TIMELINE_API               = 'https://legy-jp.line.naver.jp/mh/api'
+    LINE_TIMELINE_MH                = 'https://legy-jp.line.naver.jp/mh'
 
-    LINE_LOGIN_QUERY_PATH       = '/api/v4p/rs'
-    LINE_AUTH_QUERY_PATH        = '/api/v4/TalkService.do'
+    LINE_LOGIN_QUERY_PATH           = '/api/v4p/rs'
+    LINE_AUTH_QUERY_PATH            = '/api/v4/TalkService.do'
 
-    LINE_API_QUERY_PATH_FIR     = '/S4'
-    LINE_POLL_QUERY_PATH_FIR    = '/P4'
-    LINE_CALL_QUERY_PATH        = '/V4'
-    LINE_CERTIFICATE_PATH       = '/Q'
-    LINE_CHAN_QUERY_PATH        = '/CH4'
-    LINE_SQUARE_QUERY_PATH      = '/SQS1'
-    LINE_SHOP_QUERY_PATH        = '/SHOP4'
+    LINE_API_QUERY_PATH_FIR         = '/S4'
+    LINE_POLL_QUERY_PATH_FIR        = '/P4'
+    LINE_API_QUERY_PATH_SEC         = '/F4'
+    LINE_POLL_QUERY_PATH_SEC        = '/E4'
+    LINE_POLL_QUERY_PATH_THI        = '/H4'
+    LINE_NORMAL_POLL_QUERY_PATH     = '/NP4'
+    LINE_COMPACT_MESSAGE_QUERY_PATH = '/C5'
+    LINE_CALL_QUERY_PATH            = '/V4'
+    LINE_CERTIFICATE_PATH           = '/Q'
+    LINE_CHAN_QUERY_PATH            = '/CH4'
+    LINE_SQUARE_QUERY_PATH          = '/SQS1'
+    LINE_SHOP_QUERY_PATH            = '/SHOP4'
 
     CHANNEL_ID = {
         'LINE_TIMELINE': '1341209950',
